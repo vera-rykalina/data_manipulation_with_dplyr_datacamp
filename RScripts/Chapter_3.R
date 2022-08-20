f9035b88dd0793b834f05581788ae967e14f058a
vera-rykalina <verarykalina@gmail.com>
2022-08-20 21:19:29 +0200

    Add Chapter 3

---
+++ RScripts/Chapter_3.R
@@ -0,0 +1,8 @@
+#### Data Manipulation with dplyr ####
+# DataCamp (the instructor prefers to remain anonymous)
+# Date Created: 11.05.2022
+# Chapter 3 - Selecting and Transforming Data
+
+# Load libraries
+pacman::p_load(tidyverse)
+counties
