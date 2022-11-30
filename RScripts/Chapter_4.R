4ef54e8d54c396b72d17150b9766697f2fab9bce
vera-rykalina <verarykalina@gmail.com>
2022-11-30 21:06:30 +0100

    Add normalization

--- RScripts/Chapter_4.R
+++ RScripts/Chapter_4.R
@@ -60,4 +60,23 @@ babynames %>%
   group_by(name) %>%
   slice_max(fraction, n = 1)
 
+# Adding the total and maximum for each name
+# In the video, you learned how you could group by the year and use mutate() to add a total for that year.
+
+# In these exercises, you'll learn to normalize by a different, but also interesting metric: you'll divide each name by the maximum for that name. This means that every name will peak at 1.
+
+# Once you add new columns, the result will still be grouped by name. This splits it into 48,000 groups, which actually makes later steps like mutates slower.
+
+# Use a grouped mutate to add two columns:
+# name_total, with the sum of the number of babies born with that name in the entire dataset.
+# name_max, with the maximum number of babies born in any year.
+# Add another step to ungroup the table.
+# Add a column called fraction_max containing the number in the year divided by name_max.
+
+babynames %>%
+  group_by(name) %>%
+  mutate(name_total = sum(number),
+         name_max = max(number)) %>%
+  ungroup() %>%
+  mutate(fraction_max = number/name_max)
 
