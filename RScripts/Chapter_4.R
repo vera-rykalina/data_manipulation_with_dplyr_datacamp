bca62d4d5d71e9fe6a6e78403fc7e5642f532e18
vera-rykalina <verarykalina@gmail.com>
2022-11-30 21:15:00 +0100

    Visualize the normalized popularity of each name

--- RScripts/Chapter_4.R
+++ RScripts/Chapter_4.R
@@ -80,3 +80,23 @@ babynames %>%
   ungroup() %>%
   mutate(fraction_max = number/name_max)
 
+# Visualizing the normalized change in popularity
+# You picked a few names and calculated each of them as a fraction of their peak. This is a type of "normalizing" a name, where you're focused on the relative change within each name rather than the overall popularity of the name.
+
+# In this exercise, you'll visualize the normalized popularity of each name. Your work from the previous exercise, names_normalized, has been provided for you.
+
+names_normalized <- babynames %>%
+  group_by(name) %>%
+  mutate(name_total = sum(number),
+         name_max = max(number)) %>%
+  ungroup() %>%
+  mutate(fraction_max = number/name_max)
+
+# Filter the names_normalized table to limit it to the three names Steven, Thomas, and Matthew.
+# Create a line plot to visualize fraction_max over time, colored by name.
+
+names_filtered <- names_normalized %>%
+  filter(name %in% c("Steven", "Thomas", "Matthew"))
+
+ggplot(names_filtered, aes(x = year, y = fraction_max, color = name)) +
+  geom_line()
